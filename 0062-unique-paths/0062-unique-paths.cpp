8cb55b5702d5e068905ab351a4187180176dc8b7
Anuj Kumar <anujkr731@gmail.com>
2026-07-05 23:29:07 +0530

    Time: 3 ms (9.58%), Space: 9.6 MB (5.02%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int f(int i, int j, int m, int n) {
+        if(i == m - 1 && j == n - 1) return 1;
+        if(i >= m || j >= n) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int down = f(i + 1, j, m, n);
+        int right = f(i, j + 1, m, n);
+
+        return dp[i][j] = down + right;
+    }
+
+    int uniquePaths(int m, int n) {
+        dp.resize(m, vector<int>(n, -1));
+        return f(0, 0, m, n);
+    }
+};
