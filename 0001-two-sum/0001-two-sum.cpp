42cb8bf6a7b8487e487d59af064fb5176c556c55
Anuj Kumar <anujkr731@gmail.com>
2026-07-23 21:04:59 +0530

    Time: 2 ms (72.2%), Space: 14.8 MB (56.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,7 +5,7 @@ public:
         unordered_map<int, int> mpp;
         for(int i = 0; i < n; i++) {
             int need = target - nums[i];
-            if(mpp.find(need) != mpp.end()) return {mpp[need], i};
+            if(mpp.count(need)) return {mpp[need], i};
             mpp[nums[i]] = i;
         }
         return {-1, -1};
